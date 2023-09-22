762392c38402a8f5a043ff72929c87413c3341f9
Yash Tiwari <yash22012002@gmail.com>
2023-09-22 18:05:11 +0530

    server

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from "react";
 
-import {BrowserRouter as Router,Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Join from './components/join/Join';
+import Join from "./components/join/Join";
 
-import Chat from './components/chat/Chat';
+import Chat from "./components/chat/Chat";
 import ParticleBackground from "./components/Particle/Particle";
 
-const App = () =>{
-    return(
-        
+const App = () => {
+  return (
     <Router>
-        <div style={{position:'absolute'}}>
-            <ParticleBackground/>
-        </div>
-        < Route path="/" exact component={Join}/>
-        < Route path="/chat"  component={Chat}/>
+      <div style={{ position: "absolute" }}>
+        <ParticleBackground />
+      </div>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
     </Router>
-    );
-    }
+  );
+};
 export default App;
--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,79 +1,79 @@
-import React,{useState,useEffect} from 'react';
-import queryString from 'query-string';
-import io from 'socket.io-client';
-import './Chat.css';
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+import "./Chat.css";
 import InfoBar from "../Infobar/InfoBar";
 import Input from "../Input/Input";
 import Messages from "../Messages/Messages";
 import TextContainer from "../TextContainer/TextContainer";
-import ParticleBackground from '../Particle/Particle';
 
 let socket;
 
-const Chat=({location})=> {
-    const [name,setName]=useState('');
-    const [room, setRoom]=useState('');
-    const [users,setUsers]=useState('');
-    const [message,setMessage]=useState('');
-    const [messages,setMessages]=useState([]);
+const Chat = ({ location }) => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
 
-    const ENDPOINT='https://chat-appyash.herokuapp.com';
-    useEffect(()=>{
-        const {name,room}=queryString.parse(location.search);
+  const ENDPOINT = "localhost:5000";
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    console.log(name);
+    console.log(room);
+    socket = io(ENDPOINT, {
+      cors: {
+        origin: "https://chat-appyash.herokuapp.com",
+        credentials: true,
+      },
+      transports: ["websocket"],
+    });
 
-        socket= io(ENDPOINT, {  
-            cors: {
-            origin: "https://chat-appyash.herokuapp.com",
-            credentials: true
-          }, transports : ['websocket'] });
+    setName(name);
+    setRoom(room);
+    socket.emit("join", { name, room }, (error) => {
+      if (error) {
+        alert(error);
+      }
+    });
 
-        setName(name);
-        setRoom(room);
-        socket.emit('join',{name,room},(error)=>{
-            if(error)
-            {
-                alert(error);
-            }
-        }
-        );
+    console.log(socket);
+    return () => {
+      socket.disconnect();
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
 
-        console.log(socket);
-        return ()=>{
-            socket.disconnect();
-            socket.off();
-        }
-    },[ENDPOINT, location.search]);
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+  }, [messages]);
 
-    useEffect(()=>{
-        socket.on('message',message=>{
-            setMessages([...messages,message]);
-        });
-        socket.on("roomData", ({ users }) => {
-            setUsers(users);
-        });
-    },[messages]);
-
-
-const sendMessage=(event)=>{
+  const sendMessage = (event) => {
     event.preventDefault();
-    if(message){
-        socket.emit('sendMessage',message,()=> setMessage(''));
+    if (message) {
+      socket.emit("sendMessage", message, () => setMessage(""));
     }
-}
-console.log(message, messages,"dkfkfjd");
-    return (
-        
-            <div className="outerContainer">
-                <div className="container">
-                    
-                    <InfoBar room={room}/>
-                    <Messages messages={messages} name={name}/>
-                    <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
-
-                </div>
-                <TextContainer users={users}/>
-            </div>
-        )
-}
+  };
+  console.log(message, messages, "dkfkfjd");
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+      <TextContainer users={users} />
+    </div>
+  );
+};
 
 export default Chat;
